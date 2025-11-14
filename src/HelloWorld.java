ecec28d257f51017e21aa863448afac846a586ab
大内椋斗 <@192.168.11.22>
2025-11-14 21:13:00 +0900

    FizzBuzz作成

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,24 @@
 
 public class HelloWorld {
 
+	// 列挙型
+
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello World!");
+		 for(int a = 1; a <= 100; a++) {
+			 if (a % 3 == 0 && a % 5 == 0) {
+				 System.out.println("FizzBuzz");
+			 }
+			 else if (a % 3 == 0) {
+				 System.out.println("Fizz");
+			 }
+			 else if (a % 5 == 0) {
+				 System.out.println("Buzz");
+			 }
+	         else {
+	        	 	System.out.println(a);
+	         }
+		 }
+		}
 	}
 
-}
+
